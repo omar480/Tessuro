ae4678aa3e9d28e9e442053942ed973b964739f8
Omar <omarr.perez2020@gmail.com>
2020-05-10 18:52:39 -0700

    added shared preferences for user

--- app/src/main/java/com/csulb/tessuro/models/UserModel.java
+++ app/src/main/java/com/csulb/tessuro/models/UserModel.java
@@ -1,37 +1,66 @@
 package com.csulb.tessuro.models;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class UserModel {
     private String fullname;
     private String email;
     private String role;
+    private String imgUrl;
 
-    public UserModel(String fullname, String email, String role) {
-        this.fullname = fullname;
-        this.email = fullname;
-        this.role = role;
+    private SharedPreferences sharedPreferences;
+
+    public UserModel(SharedPreferences sharedPreferences) {
+        this.sharedPreferences = sharedPreferences;
+    }
+
+    public void setUser(String fullname, String email, String role) {
+        SharedPreferences.Editor editor = this.sharedPreferences.edit();
+        editor.putString("fullname", fullname);
+        editor.putString("email", email);
+        editor.putString("role", role);
+        editor.apply();
     }
 
     public void setFullname(String fullname) {
-        this.fullname = fullname;
+        SharedPreferences.Editor editor = this.sharedPreferences.edit();
+        editor.putString("fullname", this.fullname);
+        editor.apply();
     }
 
     public void setEmail(String email) {
-        this.email = email;
+        SharedPreferences.Editor editor = this.sharedPreferences.edit();
+        editor.putString("email", this.email);
+        editor.apply();
     }
 
     public void setRole(String role) {
-        this.role = role;
+        SharedPreferences.Editor editor = this.sharedPreferences.edit();
+        editor.putString("role", this.role);
+        editor.apply();
+    }
+
+    public void setImgUrl(String imgUrl) {
+        SharedPreferences.Editor editor = this.sharedPreferences.edit();
+        editor.putString("imgUrl", this.imgUrl);
+        editor.apply();
     }
 
     public String getFullname() {
-        return fullname;
+        System.out.println("called");
+        return this.sharedPreferences.getString("fullname", "");
     }
 
     public String getEmail() {
-        return email;
+        return this.sharedPreferences.getString("email", "");
     }
 
     public String getRole() {
-        return role;
+        return this.sharedPreferences.getString("role", "");
+    }
+
+    public String getImgUrl() {
+        return this.sharedPreferences.getString("imgUrl", "");
     }
 }
