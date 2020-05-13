72c20e1e5f61f78e3a77aea39ca00ff0a9963378
BharooM <54654778+BharooM@users.noreply.github.com>
2020-05-12 21:50:59 -0700

    Attempt to fetch Quiz data from Firestore

--- app/src/main/java/com/csulb/tessuro/views/dashboard/home/HomeStudentFragment.java
+++ app/src/main/java/com/csulb/tessuro/views/dashboard/home/HomeStudentFragment.java
@@ -1,27 +1,45 @@
 package com.csulb.tessuro.views.dashboard.home;
 
+import android.content.Context;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.csulb.tessuro.R;
+import com.csulb.tessuro.models.UserModel;
 import com.csulb.tessuro.utils.DialogUtils;
 
+import com.csulb.tessuro.utils.QuizUtils;
+import com.csulb.tessuro.views.dashboard.help.HelpFragment;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Objects;
+
+import static androidx.constraintlayout.widget.Constraints.TAG;
 
 public class HomeStudentFragment extends Fragment
 {
 
     private TextInputLayout email_editText;
     private TextInputLayout key_editText;
+    private FirebaseAuth auth;
+
+    private MaterialButton submit_Button;
 
     ImageButton imageButton;
 
@@ -52,9 +70,47 @@ public class HomeStudentFragment extends Fragment
                 dialogUtils.showDialog();
             }
         });
-          return v;
+
+        handleNextStep();
+        return v;
     }
 
+    private void handleNextStep() {
+        submit_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                try {
+                    String quizKey = key_editText.getEditText().getText().toString();
+                    String email = email_editText.getEditText().getText().toString();
+
+                    Toast.makeText(getContext(), "success?", Toast.LENGTH_SHORT).show();
+                    fetchQuizData(quizKey, email);
 
 
+                }catch (Exception e) {
+                    Log.e(TAG, "onClick: handleNextStep -> " + e.getMessage());
+                }
+            }
+        });
+    }
+
+    private void fetchQuizData(final String key, final String email){
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        firestore
+                .collection("quizzes")
+                .document(key)
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        Log.i(TAG,"Firestore got Key info successfully");
+
+                        String getKey = Objects.requireNonNull(documentSnapshot.get("quizKey")).toString();
+
+
+
+                    }
+                });
+    }
 }
